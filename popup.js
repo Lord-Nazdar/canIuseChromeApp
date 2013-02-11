b2aedfffa220dee2e95aecfca002775534fdf4a1
Thomas Poulet <dark.flames0@gmail.com>
2013-02-11 12:27:58 +0100

    Update popup.js
    *Google Analytics
    *Autocomplete

--- popup.js
+++ popup.js
@@ -1,4 +1,38 @@
 document.onkeypress = returnKey;
+
+document.onLoad=makeList();
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-32783592-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+function trackButton(e) {
+	_gaq.push(['_trackEvent', $(e.target).text(), 'clicked']);
+};
+
+function makeList(){
+
+	$.ajax({  
+		url: 'https://raw.github.com/Fyrd/caniuse/9dd42e925a555c9407f8817c56159b86a4a0e01f/data.json',  
+		dataType: 'json',  
+		async: true,  
+		success: function(json){  
+			$.each(json.data, function(i, v) {
+			  	if (v.title.search(new RegExp("","i")) != -1) {
+			  		$('<option value=\"'+v.title+'\">').appendTo('#dataList');
+				}
+			});
+		}  
+	}); 
+}
+
+
 function returnKey(evt)
 {
 	if(evt.keyCode == 13)
@@ -12,7 +46,6 @@ function returnKey(evt)
 
 
 var searchResult=new Array();
-
 
 function protectParenthesis(string){
 	string = string.replace("(", "\\\(", "gi");
@@ -46,6 +79,8 @@ function searchVal(val){
 			 		$(idName).bind('click', function() {
 					 	searchVal(v.title);
 					});
+					
+					$(idName).bind('click', trackButton);
 					
 					j++;
 				}
